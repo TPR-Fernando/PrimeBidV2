7c54b725ccdf9d5edabcd5cc25fcf8663b395261
Rovin Fernando <135942955+TPR-Fernando@users.noreply.github.com>
2024-10-29 08:05:16 +0530

    Made changes to the auctionPicture.cs model due to errors

--- PrimeBidAPI/Models/Auctionpicture.cs
+++ PrimeBidAPI/Models/Auctionpicture.cs
@@ -1,14 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PrimeBidAPI.Models
 {
     public class Auctionpicture
     {
-        [Required]
-        public IFormFile file { get; set; } // This is your uploaded file
+        [Key]
+        public int Id { get; set; } // Primary key
 
-        [Required]
-        public string FileName { get; set; } // This is the file name (e.g., image.jpg)
+        [NotMapped]
+        public IFormFile file { get; set; } // Not mapped to the database, used only for file upload
 
+        public string FileName { get; set; } // Stores the file name, e.g., "image.jpg"
+
+        public byte[] FileData { get; set; } // Store file content as binary data (optional)
+
+        // Alternatively, use a file path if storing files in the file system
+        // public string FilePath { get; set; } // Path to where the file is saved
     }
 }
